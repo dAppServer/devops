f817615e4844fd58e379ecec43db3eecba66a561
snider <snider@lethean.io>
2021-06-16 23:47:12 +0100

    adds a test script and fixes migrating tags from gitlab to docker hub

---
+++ build-test/tag-images.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export CI_REGISTRY_IMAGE='lthn.io/projects/sdk/build'
+export CI_REGISTRY='registry.gitlab.com'
+export DOCKER_REGISTRY='docker.io'
+export DOCKER_REGISTRY_IMAGE='lthn/build'
+
+docker pull ${CI_REGISTRY}/${CI_REGISTRY_IMAGE} -a
+docker images ${DOCKER_REGISTRY_IMAGE} --filter "since=${DOCKER_REGISTRY}/${DOCKER_REGISTRY_IMAGE}"  --format "docker tag {{.Repository}}:{{.Tag}} ${DOCKER_REGISTRY}/{{.Repository}}:{{.Tag}} | docker push ${DOCKER_REGISTRY}/{{.Repository}}:{{.Tag}}" | bash
