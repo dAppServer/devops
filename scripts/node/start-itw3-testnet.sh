136e089201c636bc17402165bffaa3e4c2d20009
Snider <snider@lt.hn>
2023-01-08 18:56:21 +0000

    Update start-itw3-testnet.sh

--- scripts/node/start-itw3-testnet.sh
+++ scripts/node/start-itw3-testnet.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 
-~/blockchain/bin/letheand --rpc-bind-ip=0.0.0.0 --p2p-bind-ip=0.0.0.0 --data-dir=~/data/itw3/testnet
+$(HOME)/blockchain/bin/letheand --rpc-bind-ip=0.0.0.0 --p2p-bind-ip=0.0.0.0 --data-dir=$(HOME)/data/itw3/testnet
